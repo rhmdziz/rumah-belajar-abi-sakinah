714fddf085533b110ba5ff18c5d2cd55e22c0524
rhmdziz <rahmadaziz288@gmail.com>
2023-12-03 04:30:33 +0700

    add owlcarousel for testimonial

---
+++ umar/theme/static/js/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+        margin: 10,
+        nav: false,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            700: {
+                items:2
+            },
+            1200: {
+                items: 3
+            },
+
+        }
+    })
+})
